0a1397a06ecd56e9596260449386f68f49b6d6ae
gulsah-altiparmak <42993752+gulsah-altiparmak@users.noreply.github.com>
2021-01-30 21:59:34 +0300

    Initial Commit

---
+++ Turkcell_FinalProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Turkcell_FinalProjesi
+//
+//  Created by Gulsah Altiparmak on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
